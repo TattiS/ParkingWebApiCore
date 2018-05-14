580e83310d365af1b97dea28099cd8e6ed970705
TXS81 <ihr2015.16@gmail.com>
2018-05-14 13:22:54 +0300

    Add ParkingManagerService to ParkingWebApi

--- ParkingWebApi/ParkingWebApi/Controllers/CarsController.cs
+++ ParkingWebApi/ParkingWebApi/Controllers/CarsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ParkingWebApi.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class CarsController : Controller
     {
         // GET api/values
         [HttpGet]
---
+++ ParkingWebApi/ParkingWebApi/Services/ParkingManagerService.cs
@@ -0,0 +1,14 @@
+﻿using ParkingLibrary.Classes;
+
+namespace ParkingWebApi.Services
+{
+    public class ParkingManagerService
+    {
+        public ParkManagerHelper helper { get; private set; }
+        public ParkingManagerService()
+        {
+            helper = new ParkManagerHelper();
+        }
+
+    }
+}
